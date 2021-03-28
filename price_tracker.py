dd9b775a576953dcb8d45831d2eaf57a0983153d
Logan Bynes <logan.bynes@gmail.com>
2021-03-27 18:08:13 -0600

    Add VWAP calculations

--- price_tracker.py
+++ price_tracker.py
@@ -1,13 +1,16 @@
-import requests
 import json
+import requests
+import math
+import dateutil.tz
+import datetime
 
 ONE_MINUTE = '1m'
 FIVE_MINUTES = '5m'
 ONE_HOUR = '1h'
 ONE_DAY = '1d'
 
-def get_candlestick_data(headers, symbol, interval):
-    url = 'https://api.binance.us/api/v3/klines?symbol={}&interval={}'.format(symbol, interval)
+def get_candlestick_data(headers, symbol, interval, start_time):
+    url = 'https://api.binance.us/api/v3/klines?symbol={}&interval={}&startTime={}&limit=1000'.format(symbol, interval, start_time)
     response = requests.get(url, headers=headers)
     return response.json()
 
@@ -19,7 +22,35 @@ def create_price_volume_list(candlestick_data):
     return final_list
 
 def calc_vwap(price_volume_list):
-    return
+    pvl = create_price_volume_list(price_volume_list)
+    pvl = calc_cumulative_price(pvl)
+    pvl = calc_cumulative_volume(pvl)
+    for dp in pvl:
+        dp['vwap'] = dp['cumulative_price']/dp['cumulative_volume']
+    return pvl
+
+def calc_cumulative_price(price_volume_list):
+    for i,dp in enumerate(price_volume_list):
+        if i == 0:
+            dp['cumulative_price'] = dp['avg_price']*dp['volume']
+        else:
+            dp['cumulative_price'] = (dp['avg_price']*dp['volume']) + price_volume_list[i-1]['cumulative_price']
+    return price_volume_list
+
+def calc_cumulative_volume(price_volume_list):
+    for i,dp in enumerate(price_volume_list):
+        if i == 0:
+            dp['cumulative_volume'] = dp['volume']
+        else:
+            dp['cumulative_volume'] = dp['volume'] + price_volume_list[i-1]['cumulative_volume']
+    return price_volume_list
+
+def get_open_time_utc_milliseconds():
+    six_am = (datetime.datetime
+                .now(dateutil.tz.gettz('America/Denver'))
+                .replace(hour=6, minute=0, second=0, microsecond=0)
+                .astimezone(dateutil.tz.tzutc()))
+    return math.floor(six_am.timestamp())*1000
 
 with open('config.json') as config_file:
     config = json.load(config_file)
@@ -27,7 +58,8 @@ with open('config.json') as config_file:
 headers = {
     'X-MBX-APIKEY': config['api_key']
 }
-
-data = get_candlestick_data(headers, 'BTCUSDT', FIVE_MINUTES)
-pvl = create_price_volume_list(data)
-print(pvl)
+day_open = get_open_time_utc_milliseconds()
+data = get_candlestick_data(headers, 'ETHUSD', FIVE_MINUTES, day_open)
+print(data[-5:])
+pvl = calc_vwap(data)
+print(pvl[-5:])
